a8a4095defec1ccc140fffc0eeb6711bf578f350
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2021-08-14 17:53:40 -0800

    nav

--- src/Components/Navigation/NavBar.jsx
+++ src/Components/Navigation/NavBar.jsx
@@ -3,17 +3,19 @@ import cup from '../../Images/coffee_icon.png'
 import { IconButton,Badge } from '@material-ui/core';
 import { ShoppingCart,ShoppingBasket } from '@material-ui/icons';
 
+import useStyles from './style'
 
 const NavBar = () => {
+    const classes=useStyles();
     return ( 
         <div className="Nav-container">
             <div className="logo">
-                <div className="imgbx"><img src={cup} alt="img"/></div>
+                <img src={cup} alt="img"/>
                 <h4>Coding Tee</h4>
             </div>
             <div className="cart-btn">
                 <IconButton aria-label="show cart item" color="inherit">
-                    <Badge badgeContent={2}>
+                    <Badge badgeContent={5}>
                         <ShoppingCart/>
                     </Badge>
                 </IconButton>
---
+++ src/Components/Navigation/style.js
@@ -0,0 +1,15 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(()=>({
+
+    NavContainer:{
+        width: "100%",
+        height: "30%",
+        display: "flex",
+        justifyContent: "space-between",
+        aligniItems: "center",
+        padding: "5px 10px",
+        border: "2px solid rgba(0, 0, 0, .09)",
+    }
+
+}));
