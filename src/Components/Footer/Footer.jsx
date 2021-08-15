7d9f38356a9e6ddc0a51f6bee690f5c9740ab97b
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2021-08-15 07:48:13 +0200

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import useStyles from './styles'
 import { WbSunnyOutlined,Brightness2Outlined } from '@material-ui/icons';
+import { Formik,Form } from 'formik';
+import * as Yup from 'yup';
+import TextField from './Subcribe/TextField';
+
 
 
 
@@ -8,10 +12,38 @@ const Footer = () => {
     return ( 
         <div className="footerContainer">
             <div className="SubForm">
+            <Formik
+                initialValues={{
+                    name:'',
+                    surname:'',
+                    age:'',
+                    dob:'',
+                    location:''
+                }}
+            >
+                {formik=>(
+                    <div className="form">
+                        <h2>Subcribe</h2>
+                        <p>Sign up to get the latest on sales,new releases and more...</p>
+                        <Form>
+                            <TextField label="First Name" name="name" type="text"/>
+                            <TextField label="Your Email" name="name" type="text"/>
+                            <div className="formbtnbx">
+                                <button>Sign up</button>
+                            </div>
+                        </Form>
+                    </div>
+                )}
+            </Formik>
 
             </div>
-            <div className="conatact">
-
+            <div className="contactInfo">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+                <i class="fa fa-linkedin" aria-hidden="true"></i>
+                <i class="fa fa-github" aria-hidden="true"></i>
+                <i class="fa fa-whatsapp" aria-hidden="true"></i>
             </div>
             <div className="copyRights">
                 <div>
---
+++ src/Components/Footer/Subcribe/TextField.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ErrorMessage,useField } from 'formik';
+
+const TextField = ({label,...props}) => {
+    const [field,meta]=useField(props);
+    return (  
+        <div>
+            <input {...field}{...props} placeholder={label} />
+            <ErrorMessage name={field.name} component="div" className="error"/>
+        </div>
+    );
+}
+ 
+export default TextField;
