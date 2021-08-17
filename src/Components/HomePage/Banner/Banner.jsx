f16bbc4f10984bd17d7b2eee4b530c039b105cb0
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2021-08-16 20:21:49 -0800

    banner

--- src/Components/HomePage/Banner/Banner.jsx
+++ src/Components/HomePage/Banner/Banner.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 
 
 const Banner = () => {
-    return ( <div className="bannerContainer flexCenter">you like to code? <br />it is time to show off your super powers</div> );
+    return ( 
+            <div className="bannerContainer flexCenter">
+                <div className="backgroundImg">
+                    <img src={'https://images.unsplash.com/photo-1524666643752-b381eb00effb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=751&q=80'} alt="" />
+                </div>
+                <div className="content">
+                    you like to code? 
+                    <br />
+                    it is time to show off your super powers
+                </div>
+                <div className="ImgCover"/>
+            </div> );
 }
  
 export default Banner;
